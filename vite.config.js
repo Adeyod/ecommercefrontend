073634cb49348bf41a8a288be0abe8547c10e4ab
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2023-10-12 22:35:10 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), pluginRewriteAll()],
+
+  // added
+  // build: {
+  //   // Specify your entry point
+  //   rollupOptions: {
+  //     input: '/src/main.jsx', // Replace with the actual path to your entry file
+  //   },
+  // },
+});
